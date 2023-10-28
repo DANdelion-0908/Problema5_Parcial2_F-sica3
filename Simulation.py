a2ddf36fbde11e9ea18bbf84629fd6868b6a60dd
chuy-zip <andreschuy03@gmail.com>
2023-10-27 22:39:14 -0600

    added drawings and simulation class

---
+++ Simulation.py
@@ -0,0 +1,7 @@
+from drawings import *
+
+turtle.screensize(1200,800)
+
+t = turtle.Turtle()
+
+Sphere(50)
---
+++ drawings.py
@@ -0,0 +1,71 @@
+import turtle
+import decimal
+
+def DrawCircle(tr: turtle, height: float, width: float, outline: float, color: str):
+    tr.pensize(outline)
+    if(color != ""):
+        tr.pencolor(color)
+
+    tr.shape("circle")
+    tr.shapesize(height, width, outline)
+    tr.fillcolor(color)
+
+def Sphere(radius: float):
+    #Initial setup
+    t1 = turtle.Turtle()
+    t1.pensize(1)
+    t1.penup()
+    t1.goto(0, -1*radius)
+    t1.pendown()
+
+    #Drawing half of the circle
+    t1.circle(radius, 180)
+
+    #Painting half of the circle
+    t1.fillcolor("Blue")
+    t1.begin_fill()
+    t1.circle(radius, 180)
+    t1.end_fill()
+    t1.penup()
+
+    #Positioning pen to the circle's center
+    t1.backward(radius)
+    t1.left(90)
+    t1.forward(radius)
+    t1.right(90)
+    t1.forward(radius)
+    t1.pendown()
+
+    #Making the inner circle
+    DrawCircle(t1, radius/10, radius/10 * 0.20, 0.5, "darkblue")
+
+    turtle.exitonclick()
+
+#Function to draw a grid
+def draw_grid(step, size,turtle):
+    for i in range(-size, (size+1), step):
+        turtle.penup()
+        turtle.goto(i, -size)
+        turtle.pendown()
+        turtle.goto(i, size)
+        
+    for i in range(-size, (size+1), step):
+        turtle.penup()
+        turtle.goto(-size, i)
+        turtle.pendown()
+        turtle.goto(size, i)
+
+#Funcion principal para test
+
+
+radius: decimal = 30
+
+Sphere(radius)
+
+
+
+
+
+
+
+
